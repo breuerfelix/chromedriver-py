07256d453cb54b34f31faeaf3a023a99a9a5e183
Felix Breuer <f.breuer94@gmail.com>
2019-01-09 10:55:15 +0100

    comment specific version

--- updater.py
+++ updater.py
@@ -189,7 +189,8 @@ print('current version: ' + str(current_version))
 
 version, _ = check_for_update(current_version)
 
-version = '2.44'
+# static number to publish specific version
+#version = '2.44'
 
 if not version:
     sys.exit(1)
